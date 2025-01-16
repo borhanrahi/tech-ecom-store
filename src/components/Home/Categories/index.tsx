946cf3ab5301e2da7ee664de8f605c5f2e868ba4
borhanrahi <borhanrahi111@gmail.com>
2025-01-16 23:12:21 +0600

    fix

--- src/components/Home/Categories/index.tsx
+++ src/components/Home/Categories/index.tsx
@@ -1,38 +1,27 @@
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { useCallback, useRef, useEffect } from "react";
+import type { Swiper as SwiperType } from "swiper"; // Import Swiper type explicitly
+import { useCallback, useRef } from "react";
 import data from "./categoryData";
-import Image from "next/image";
-
-// Import Swiper styles
-import "swiper/css/navigation";
 import "swiper/css";
 import SingleItem from "./SingleItem";
 
 const Categories = () => {
-  const sliderRef = useRef(null);
+  const sliderRef = useRef<SwiperType | null>(null); // Use SwiperType here
 
   const handlePrev = useCallback(() => {
-    if (!sliderRef.current) return;
-    sliderRef.current.swiper.slidePrev();
+    sliderRef.current?.slidePrev();
   }, []);
 
   const handleNext = useCallback(() => {
-    if (!sliderRef.current) return;
-    sliderRef.current.swiper.slideNext();
-  }, []);
-
-  useEffect(() => {
-    if (sliderRef.current) {
-      sliderRef.current.swiper.init();
-    }
+    sliderRef.current?.slideNext();
   }, []);
 
   return (
     <section className="overflow-hidden pt-17.5">
       <div className="max-w-[1170px] w-full mx-auto px-4 sm:px-8 xl:px-0 pb-15 border-b border-gray-3">
         <div className="swiper categories-carousel common-carousel">
-          {/* <!-- section title --> */}
+          {/* Section title */}
           <div className="mb-10 flex items-center justify-between">
             <div>
               <span className="flex items-center gap-2.5 font-medium text-dark mb-1.5">
@@ -117,21 +106,12 @@ const Categories = () => {
           </div>
 
           <Swiper
-            ref={sliderRef}
+            onSwiper={(swiper) => (sliderRef.current = swiper)}
             slidesPerView={6}
             breakpoints={{
-              // when window width is >= 640px
-              0: {
-                slidesPerView: 2,
-              },
-              1000: {
-                slidesPerView: 4,
-                // spaceBetween: 4,
-              },
-              // when window width is >= 768px
-              1200: {
-                slidesPerView: 6,
-              },
+              0: { slidesPerView: 2 },
+              1000: { slidesPerView: 4 },
+              1200: { slidesPerView: 6 },
             }}
           >
             {data.map((item, key) => (
